9016b7d99ac50f57a2c4f361cb2279e5c3bdbcbe
salimboughanmi <boughanmi1salim@gmail.com>
2024-02-28 15:53:10 +0100

    add credit cart credentiel 2

--- payment_project/lib/Feature/Checkout/presentation/Views/payment_details.dart
+++ payment_project/lib/Feature/Checkout/presentation/Views/payment_details.dart
@@ -23,13 +23,10 @@ class CustomCreditCart extends StatefulWidget {
 }
 
 class _CustomCreditCartState extends State<CustomCreditCart> {
-  String cardNumber = 'vvv',
-      expiryDate = 'vvvv',
-      cardHolderName = 'vvv',
-      cvvCode = 'vvv';
+  String cardNumber = '', expiryDate = '', cardHolderName = '', cvvCode = '';
 
   bool showBackView = true;
-
+  final GlobalKey<FormState> formKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -43,6 +40,73 @@ class _CustomCreditCartState extends State<CustomCreditCart> {
               true, //true when you want to show cvv(back) view
           onCreditCardWidgetChange:
               (value) {}, // Callback for anytime credit card brand is changed
+        ),
+        CreditCardForm(
+          formKey: formKey, // Required
+          cardNumber: cardNumber, // Required
+          expiryDate: expiryDate, // Required
+          cardHolderName: cardHolderName, // Required
+          cvvCode: cvvCode, // Required
+          //cardNumberKey: cardNumberKey,
+          //cvvCodeKey: cvvCodeKey,
+          //expiryDateKey: expiryDateKey,
+          //cardHolderKey: cardHolderKey,
+          onCreditCardModelChange: (CreditCardModel data) {}, // Required
+          obscureCvv: true,
+          obscureNumber: true,
+          isHolderNameVisible: true,
+          isCardNumberVisible: true,
+          isExpiryDateVisible: true,
+          enableCvv: true,
+          cvvValidationMessage: 'Please input a valid CVV',
+          dateValidationMessage: 'Please input a valid date',
+          numberValidationMessage: 'Please input a valid number',
+          cardNumberValidator: (String? cardNumber) {},
+          expiryDateValidator: (String? expiryDate) {},
+          cvvValidator: (String? cvv) {},
+          cardHolderValidator: (String? cardHolderName) {},
+          onFormComplete: () {
+            // callback to execute at the end of filling card data
+          },
+          autovalidateMode: AutovalidateMode.always,
+          disableCardNumberAutoFillHints: false,
+          inputConfiguration: const InputConfiguration(
+            cardNumberDecoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Number',
+              hintText: 'XXXX XXXX XXXX XXXX',
+            ),
+            expiryDateDecoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Expired Date',
+              hintText: 'XX/XX',
+            ),
+            cvvCodeDecoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'CVV',
+              hintText: 'XXX',
+            ),
+            cardHolderDecoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Card Holder',
+            ),
+            cardNumberTextStyle: TextStyle(
+              fontSize: 10,
+              color: Colors.black,
+            ),
+            cardHolderTextStyle: TextStyle(
+              fontSize: 10,
+              color: Colors.black,
+            ),
+            expiryDateTextStyle: TextStyle(
+              fontSize: 10,
+              color: Colors.black,
+            ),
+            cvvCodeTextStyle: TextStyle(
+              fontSize: 10,
+              color: Colors.black,
+            ),
+          ),
         ),
       ],
     );
