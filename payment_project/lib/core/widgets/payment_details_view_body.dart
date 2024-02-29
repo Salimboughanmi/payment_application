eb41bfbb2f376490ebd315f21dad672feffc7864
salimboughanmi <boughanmi1salim@gmail.com>
2024-02-29 20:56:52 +0100

    add payment methode

--- payment_project/lib/core/widgets/payment_details_view_body.dart
+++ payment_project/lib/core/widgets/payment_details_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:payment_project/Feature/Checkout/presentation/Views/payment_details.dart';
+import 'package:payment_project/core/widgets/custom_button.dart';
+import 'package:payment_project/core/widgets/custom_creditCart.dart';
 import 'package:payment_project/core/widgets/payment_methode_list_view.dart';
 
 class PaymentDetailsViewBody extends StatelessWidget {
@@ -7,13 +8,20 @@ class PaymentDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          PaymentMethodeListView(),
-          CustomCreditCart(),
-        ],
-      ),
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: PaymentMethodeListView(),
+        ),
+        SliverToBoxAdapter(
+          child: CustomCreditCart(),
+        ),
+        SliverFillRemaining(
+          child: CustomButton(
+            text: "Complete payment",
+          ),
+        ),
+      ],
     );
   }
 }
